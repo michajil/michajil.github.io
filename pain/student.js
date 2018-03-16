0b98c44badd6de6a42abcef8ae88784949140add
Isaac Michaelis <30627814+michajil@users.noreply.github.com>
2018-03-16 09:09:08 -0400

    Add files via upload

---
+++ pain/student.js
@@ -0,0 +1,13 @@
+var grad;
+var Submit= "<input type='text'name='boxes'>";
+
+$(function(){
+   $(".sub").click(function(){
+       Submit();
+       $(".pie").html("<input type='text'name='boxes'>");
+   });    
+     
+       
+
+    
+});
